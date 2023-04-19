32560b8f030954171fed1bca5d7645564d90111b
Johnkhay <johnkingsleyegeonu@gmail.com>
2023-04-19 05:48:05 +0100

    added Javascript to add task list from input value

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+loadEventListeners();
+
+function loadEventListeners() {
+    
+    form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask)
+}
+
+function addTask(e) {
+
+    if (taskInput.value === '') {
+        alert('add')
+    }
+
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement('a')
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<img class="fa fa-remove" src="./icon-arrow-down.svg" alt="">';
+    li.appendChild(link);
+    taskList.appendChild(li);
+    taskInput.value = '';
+
+
+ e.preventDefault();   
+}
+
+
+function removeTask(e) {
+    console.log(e.target);
+}
